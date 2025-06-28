6699b42768720990bc57eeb549d42fc5084ab572
DevKorrir <kipyegonaldo@proton.me>
2025-06-28 22:08:43 +0300

    started farm post managenment with an interface

---
+++ app/src/main/java/dev/korryr/agrimarket/ui/features/postManagement/model/FarmPostsRepository.kt
@@ -0,0 +1,33 @@
+
+package dev.korryr.agrimarket.ui.features.postManagement.model
+
+import dev.korryr.agrimarket.ui.features.posts.dataModel.dataClass.FarmPost
+import kotlinx.coroutines.flow.Flow
+
+interface FarmPostsService {
+    
+    /**
+     * Get all posts for a specific farmer
+     */
+    fun getAllPostsForFarmer(farmerId: String): Flow<List<FarmPost>>
+    
+    /**
+     * Update an existing farm post
+     */
+    suspend fun updatePost(post: FarmPost): Result<Unit>
+    
+    /**
+     * Delete a farm post
+     */
+    suspend fun deletePost(postId: String): Result<Unit>
+    
+    /**
+     * Upload a new image and return the URL
+     */
+    suspend fun uploadImage(imageUri: String): Result<String>
+    
+    /**
+     * Delete an image from storage
+     */
+    suspend fun deleteImage(imageUrl: String): Result<Unit>
+}
